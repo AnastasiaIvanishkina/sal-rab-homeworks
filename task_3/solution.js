be5e467b0130365d29cde0eea3279b38774a3ef5
Anastasia Ivanishkina <ivanishkina.nastya@mail.ru>
2022-02-20 16:02:29 +0300

    task_3 problem

--- task_3/solution.js
+++ task_3/solution.js
@@ -2,26 +2,23 @@ function renderCartItem(item) {
     const product = item;
 
     // Задание №3.1. Формирование строки корзины
+    let item = {id: id, title: title, count: count, price: price}; 
 
-    // product - объект вида {id: id, title: title, count: count, price: price}
-    // например, {id: 1, title: 'Пицца', count: 5, price: '500.00'}, где
-    // id - идентификатор
-    // title - наименование
-    // price - цена
-    // count - количество
+    let itemCountText = String();
+    let text1 = "количество";
+    let text2 = "x";
+    let text3 = "цена"
+    let text4 = "₽ ="; 
 
-    // сформируйте строку itemCountText вида:
-    // количество × цена ₽ = стоимость ₽
-    // например,
-    // 5 × 500.00 ₽ = 2500 ₽
-    // 1. Создайте переменную itemCountText, равную пустой строке
-    // 2. Присвойте переменной itemCountText результат склеивания itemCountText и количества
-    // 3. Присвойте переменной itemCountText результат склеивания itemCountText и ` × `
-    // 4. Присвойте переменной itemCountText результат склеивания itemCountText и цены
-    // 5. Присвойте переменной itemCountText результат склеивания itemCountText и ` ₽ = `
-    // 6. Создайте переменную sum и присвойте ей значение произведения количества и цены
-    // 7. Присвойте переменной itemCountText результат склеивания itemCountText и sum
-    // 8. Присвойте переменной itemCountText результат склеивания itemCountText и ` ₽`
+        itemCountText = text1 + " " + text2 + " " + text3 + " " + text4 + " ";
+
+    let productProperty1 = count; 
+    let productProperty2 = price;
+    let sum = productProperty1 * productProperty2;
+
+    let text5 = "₽";
+        
+        itemCountText = itemCountText + sum + " " + text5;
 
     // Конец решения задания №3.1.
 
