60e7e88d280a574d25ead9dcf4f652ab9567473e
Anastasia Ivanishkina <ivanishkina.nastya@mail.ru>
2022-02-23 23:25:03 +0300

    task_2 done

--- task_3/solution.js
+++ task_3/solution.js
@@ -1,24 +1,13 @@
 function renderCartItem(item) {
     const product = item;
 
-    // Задание №3.1. Формирование строки корзины
-    let item = {id: id, title: title, count: count, price: price}; 
+    // Задание №3.1. Формирование строки корзины 
 
-    let itemCountText = String();
-    let text1 = "количество";
-    let text2 = "x";
-    let text3 = "цена"
-    let text4 = "₽ ="; 
-
-        itemCountText = text1 + " " + text2 + " " + text3 + " " + text4 + " ";
-
-    let productProperty1 = count; 
-    let productProperty2 = price;
-    let sum = productProperty1 * productProperty2;
-
-    let text5 = "₽";
-        
-        itemCountText = itemCountText + sum + " " + text5;
+    let itemCountText = item.count + " " + "×" + " " + item.price + " " + "₽ =" + " ";
+    
+    let sum = item.count * item.price;
+    
+    itemCountText = itemCountText + sum + " " + "₽";
 
     // Конец решения задания №3.1.
 
