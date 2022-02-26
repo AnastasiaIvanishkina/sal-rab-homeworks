a76dcb1b82090121017041a11d1a50bc48b0b865
Anastasia Ivanishkina <ivanishkina.nastya@mail.ru>
2022-02-26 16:03:11 +0300

    task_final problem

--- task_final/solution.js
+++ task_final/solution.js
@@ -34,16 +34,22 @@
 // }
 
 function sendRequest(name, phone, address, goods, sum) {
-    let data = {goods: [], order: {}};
+
+    let addressRussian = `ул. ${address.street}, дом ${address.house}, ${address.entrance} подъезд, ${address.floor} этаж, кв ${address.flat}`;
+
+    let order = {addressRussian, sum};
+    
+    goods = [goods.title, goods.count]
+
+    let client = name + " " + phone;
+
+    let data = {client, goods, order};
 
     let countOfGoods = goods.length;
 
     for (let i = 0; i <= countOfGoods; i += 1) {
         data.goods.push(goods[i].title);
     }
-
-    data.order.address = address;
-    data.order.sum = name + phone + address + goods + sum;
 
     data.client = 'Иван';
 
